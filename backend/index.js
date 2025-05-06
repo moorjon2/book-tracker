4520edb8161924de83311876e9b43aa3eee15405
Michael Fernandez <mfernandezm85@gmail.com>
2025-05-06 14:26:51 -0400

    API routes

--- backend/index.js
+++ backend/index.js
@@ -16,6 +16,58 @@ app.get('/api/health', (request, response) => {
     response.send({ status: 'ok' });
 });
 
+// GET single book by ID
+app.get('/api/books/:id', (request, response, next) => {
+    Book.findById(request.params.id)
+        .then(book => {
+            if (book) {
+                response.json(book);
+            } else {
+                response.status(404).end();
+            }
+        })
+        .catch(error => next(error));
+});
+
+// POST new book
+app.post('/api/books', (request, response, next) => {
+    const { title, author, year, pages, status } = request.body;
+
+    const book = new Book({
+        title,
+        author,
+        year,
+        pages,
+        status,
+    });
+
+    book.save()
+        .then(savedBook => response.status(201).json(savedBook))
+        .catch(error => (error));
+});
+
+// PUT update book
+app.put('/api/books/:id', (request, response, next) => {
+    const { title, author, year, pages, status } = request.body;
+
+    const updatedBook = { title, author, year, pages, status };
+
+    Book.findByIdAndUpdate(request.params.id, updatedBook, {
+        new: true,
+        runValidators: true,
+        context: 'query',
+    })
+        .then(result => response.json(result))
+        .catch(error => next(error));
+});
+
+// DELETE book
+app.delete('/api/book/:id', (request, response, next) => {
+    Book.findByIdAndDelete(request.params.id)
+        .then(() => response.status(204).end())
+        .catch(error => next(error));
+});
+
 // Start sever
 const PORT =  process.env.PORT || 3000;
 app.listen(PORT, () => {
